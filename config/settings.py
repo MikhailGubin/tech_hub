8bfacf7e03e12d1e0a9095f276c4196e9d59f66d
Mikhail Gubin <mgubin82@gmail.com>
2025-09-16 22:35:19 +0300

    Changed model User. Added url users in config/urls.py Added settings for user in settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -85,6 +86,20 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
--- users/models.py
+++ users/models.py
@@ -1,3 +1,76 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Переопределяю модель 'Пользователь'"""
+
+    POSITION_CHOICES = [
+        ("", "---------"),  # Пустой вариант
+        ("employee", "Сотрудник"),
+        ("team_leader", "Руководитель"),
+        ("director", "Директор"),
+    ]
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Почта", help_text="Укажите почту")
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        verbose_name="Аватар",
+        blank=True,
+        null=True,
+        help_text="Загрузите свой аватар",
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Телефон",
+        blank=True,
+        null=True,
+        help_text="Введите номер телефона",
+    )
+    city = models.CharField(
+        max_length=50,
+        verbose_name="Город",
+        blank=True,
+        null=True,
+        help_text="Введите название города",
+    )
+    position = models.CharField(
+        max_length=100,
+        choices=POSITION_CHOICES,
+        blank=True,  # Разрешаем пустое значение
+        default="",  # Значение по умолчанию
+        verbose_name="Должность",
+        help_text="Укажите должность",
+    )
+    surname = models.CharField(
+        max_length=30,
+        blank=True,
+        default="",
+        verbose_name="Фамилия",
+        help_text="Укажите Фамилию",
+    )
+    name = models.CharField(
+        max_length=30,
+        blank=True,
+        default="",
+        verbose_name="Имя",
+        help_text="Укажите имя",
+    )
+    patronymic = models.CharField(
+        max_length=30,
+        blank=True,
+        default="",
+        verbose_name="Отчество",
+        help_text="Укажите отчество",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Сотрудник"
+        verbose_name_plural = "Сотрудники"
+
+    def __str__(self):
+        return f"ФИО: {self.surname} {self.name} {self.patronymic}\nДолжность: {self.position}\nПочта: {self.email}"
