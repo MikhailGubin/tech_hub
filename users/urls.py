e983fdb5e22236c9db475a6884dc5916e8f293b4
Mikhail Gubin <mgubin82@gmail.com>
2025-09-20 11:21:05 +0300

    Added urls in urls.py in users

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,23 @@
+from django.urls import path
+from rest_framework.permissions import AllowAny
+
 from users.apps import UsersConfig
+from users.views import (
+    UserCreateAPIView,
+    UserDestroyAPIView,
+    UserListAPIView,
+    UserRetrieveAPIView,
+    UserUpdateAPIView,
+)
 
 app_name = UsersConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+    path("", UserListAPIView.as_view(), name="users-list"),
+    path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user-retrieve"),
+    path("<int:pk>/delete/", UserDestroyAPIView.as_view(), name="user-delete"),
+    path("<int:pk>/update/", UserUpdateAPIView.as_view(), name="user-update"),
+    # path("login/", CustomTokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"),
+    # path("token/refresh/", CustomTokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token-refresh"),
+]
