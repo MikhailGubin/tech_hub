75028c59c57484ef70d6883d30ca998308476fbc
Mikhail Gubin <mgubin82@gmail.com>
2025-09-24 23:06:23 +0300

    Added description to classes in admin.py in electronics

--- electronics/admin.py
+++ electronics/admin.py
@@ -7,6 +7,7 @@ from electronics.models import Contact, NetworkNode, Product
 
 @admin.register(NetworkNode)
 class NetworkNodeAdmin(admin.ModelAdmin):
+    """ Панель администратора для модели "Сетевое звено". """
     list_display = ["name", "node_type", "get_supplier_link", "debt", "created_at"]
     list_filter = ["contact__city", "node_type"]  # Фильтр по городу
     actions = ["clear_debt"]
@@ -15,6 +16,7 @@ class NetworkNodeAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', )
 
     def get_supplier_link(self, obj):
+        """ Создаёт ссылку на поставщика """
         if obj.supplier:
             url = reverse("admin:electronics_networknode_change", args=[obj.supplier.id])
             return format_html('<a href="{}">{}</a>', url, obj.supplier.name)
@@ -23,6 +25,7 @@ class NetworkNodeAdmin(admin.ModelAdmin):
     get_supplier_link.short_description = "Поставщик"
 
     def clear_debt(self, request, queryset):
+        """ Обнуляет задолженность перед поставщиком """
         updated_count = queryset.update(debt=0.00)
         self.message_user(request, f"Задолженность очищена для {updated_count} объектов.")
 
@@ -31,9 +34,11 @@ class NetworkNodeAdmin(admin.ModelAdmin):
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
+    """ Панель администратора для модели "Контакт". """
     list_display = ["email", "country", "city", "street", "house_number"]
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    """ Панель администратора для модели "Продукт". """
     list_display = ["name", "model", "release_date"]
